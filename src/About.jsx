58ce0271d083cf461bba29f16f8c3758086d5714
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-03-10 05:16:08 -0600

    Initial Commit

---
+++ src/About.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+
+export default function About() {
+	return (
+		<section className='about'>
+			<div className='background-image'></div>
+			<div className='text-content'>
+				<h1>Don't squeeze in a sedan when you could relax in a van.</h1>
+				<p>
+					Our mission is to enliven your road trip with the perfect travel van
+					rental. Our vans are recertified before each trip to ensure your
+					travel plans can go off without a hitch.
+				</p>
+				<p>(Hitch costs extra 😉)</p>
+				<p>
+					Our team is full of vanlife enthusiasts who know firsthand the magic
+					of touring the world on 4 wheels.
+				</p>
+				<div className='card'>
+					<p>Your destination is waiting.</p>
+					<p>Your van is ready.</p>
+					<Link to='/vans'>
+						<button type='button'>Explore our vans</button>
+					</Link>
+				</div>
+			</div>
+		</section>
+	);
+}
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Logo from './assets/images/logo.png';
+
+function App() {
+	return (
+		<BrowserRouter>
+			<section className='header'>
+				<Link to='/'>
+					<img src={Logo} alt='VanLife Logo' className='logo' />
+				</Link>
+				<nav>
+					<ul>
+						<li>
+							<Link to='/about'>About</Link>
+						</li>
+						<li>
+							<Link to='/vans'>Vans</Link>
+						</li>
+					</ul>
+				</nav>
+			</section>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/about' element={<About />} />
+			</Routes>
+			<section className='footer'>
+				<p>
+					<span className='copyright'>©</span> 2022 #VANLIFE
+				</p>
+			</section>
+		</BrowserRouter>
+	);
+}
+
+export default App;
