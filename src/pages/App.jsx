efc0fa4409b1bf6702bb2d632cffbcb675c902a0
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-03-13 00:47:49 -0600

    Added file structure for the code.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Home from './Home';
 import About from './About';
-import Logo from './assets/images/logo.png';
+import Logo from '../assets/images/logo.png';
 
 function App() {
 	return (
