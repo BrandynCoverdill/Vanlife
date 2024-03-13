0403eff72b6cd0bde48a4f5651c2a58306111688
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-03-13 01:05:03 -0600

    Seperated styles per component.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import '../styles/about.css';
 
 export default function About() {
 	return (
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import '../styles/vans.css';
 
 export default function Vans() {
 	return <h1>Hello, world!</h1>;
