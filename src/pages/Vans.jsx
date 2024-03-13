338bfb3fecae71603bac1b9aeb1e22c29b8197d7
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-03-13 01:27:18 -0600

    Added miragejs dependencies

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -2,5 +2,26 @@ import {Link} from 'react-router-dom';
 import '../styles/vans.css';
 
 export default function Vans() {
-	return <h1>Hello, world!</h1>;
+	return (
+		<section className='vans'>
+			<h1>Explore our van options</h1>
+			<div className='filter-container'>
+				<button type='button' className='simple-filter-btn'>
+					Simple
+				</button>
+				<button type='button' className='luxury-filter-btn'>
+					Luxury
+				</button>
+				<button type='button' className='rugged-filter-btn'>
+					Rugged
+				</button>
+				<button type='button' className='clear-filter-btn'>
+					Clear filters
+				</button>
+			</div>
+			<div className='van-list-container'>
+				{/* Map over the data to show a list of vans */}
+			</div>
+		</section>
+	);
 }
